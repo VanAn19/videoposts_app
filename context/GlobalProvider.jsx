eeffb3774d2e526a90530d49b9a5f8902b42a3f2
VanAn19 <mrfeed69@gmail.com>
2024-08-06 16:16:35 +0700

    appwrite-context

--- context/GlobalProvider.jsx
+++ context/GlobalProvider.jsx
@@ -2,12 +2,12 @@ import { View, Text } from 'react-native'
 import { createContext, useContext, useState, useEffect } from 'react'
 
 const GlobalContext = createContext();
-export const useGlobalContext() => useContext(GlobalContext);
+export const useGlobalContext = () => useContext(GlobalContext);
 
 const GlobalProvider = ({ children }) => {
   return (
     <GlobalContext.Provider>
-        
+
     </GlobalContext.Provider>
   )
 }
